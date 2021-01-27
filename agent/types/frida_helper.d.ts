586cbcb93c9cdd0528052720fd078561d73f6505
kang <kjxstar@gmail.com>
2021-01-27 19:59:35 +0800

    update frida helper

--- agent/types/frida_helper.d.ts
+++ agent/types/frida_helper.d.ts
@@ -1,6 +1,6 @@
-export type HookImpl<T> = (() => void) & {
-    implementation: any;
-    overload(...args: string[]): HookImpl<T>;
+export type HookImpl<T> = Function & {
+    implementation: Function;
+    overload(...args: string[]): FunctionProperty<T>;
 }
 
-export type FunctionProperty<T> = ((() => void) & HookImpl<T> & T)
+export type FunctionProperty<T> = (HookImpl<T> & T)
