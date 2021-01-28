63793faad3f36ead66c4f944a86cf7f6cc8529cd
kang <kjxstar@gmail.com>
2021-01-28 13:58:10 +0800

    java basic

---
+++ agent/types/java.ts
@@ -0,0 +1,17 @@
+export let jboolean = "boolean";
+export let jbyte = "byte";
+export let jchar = "char";
+export let jshort = "short";
+export let jint = "int";
+export let jlong = "long";
+export let jfloat = "float";
+export let jdouble = "double";
+
+export let jbooleanArray = "[Z"
+export let jbyteArray = "[B"
+export let jcharArray = "[C"
+export let jshortArray = "[S"
+export let jintArray = "[I"
+export let jlongArray = "[J"
+export let jfloatArray = "[F"
+export let jdoubleArray = "[D"
